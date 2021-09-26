948373462f808db2b1b093d4cd15cdba612a9d10
Sergey <honor353@gmail.com>
2021-09-26 19:53:18 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomIntNumber(min, max) {
-  if (max >= min) {
-    min = Math.ceil(Math.abs(min));
-    max = Math.floor(Math.abs(max));
+  if (max >= min && min > 0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return (Math.floor(Math.random() * (max - min + 1)) + min);
   }
   return false;
@@ -9,9 +9,7 @@ function getRandomIntNumber(min, max) {
 getRandomIntNumber();
 
 function getRandomFloatNumber(min, max, quantityConverter) {
-  if (max >= min) {
-    min = Math.abs(min);
-    max = Math.abs(max);
+  if (max >= min || min > 0) {
     const numPowerConverter = Math.pow(10, quantityConverter);
     return Math.round((Math.random() * (max - min) + min) * numPowerConverter) / numPowerConverter;
   }
